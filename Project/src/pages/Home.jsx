c9c6a022bd83f4185b7ea888c701d5ffddc86e6a
meerabalal <balalmeera17@gmail.com>
2025-05-09 06:49:50 +0545

    Landing Page

--- Project/src/pages/Home.jsx
+++ Project/src/pages/Home.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Laptop from "../pages/Laptop.jpg";
 const Home = () => {
   const nagivate = useNavigate();
   return (
-    <div className=" border-none w-200 flex flex-col m-auto mt-40 justify-center p-6 gap-3">
-      <div></div>
-      <h1 className="font-bold text-4xl">Welcome to Your BlogSpace</h1>
-      <p className="font-bold text-1xl text-yellow-700">
-        Share your thought ,stories,and ideas with the worlds
-      </p>
-      <div className="flex gap-3 my-5">
-        <button
-          onClick={() => nagivate("/login")}
-          className="text-white bg-green-700 text-center w-25 h-10 rounded-xl
+    <div className="border-none w-full  relative gap-3">
+      <div
+        className=" h-screen w-full bg-cover bg-center opacity-90 blur-xs "
+        style={{ backgroundImage: `url(${Laptop})` }}
+      ></div>
+      <div className="absolute inset-0 flex flex-col justify-center items-center">
+        <h1 className="font-bold text-4xl">Welcome to Your BlogSpace</h1>
+        <p className="font-bold text-2xl text-blue-700">
+          Share your thought ,stories,and ideas with the worlds
+        </p>
+        <div className="flex gap-3 my-5">
+          <button
+            onClick={() => nagivate("/login")}
+            className="text-white bg-green-700 text-center w-25 h-10 rounded-xl
         cursor-pointer text-1xl font-medium "
-        >
-          login
-        </button>
+          >
+            login
+          </button>
 
-        <button
-          onClick={() => nagivate("/signup")}
-          className="text-green-700 bg-amber-50 border-2 border-green-700 text-center w-25 h-10 rounded-xl
+          <button
+            onClick={() => nagivate("/signup")}
+            className="text-green-700 bg-amber-50 border-2 border-green-700 text-center w-25 h-10 rounded-xl
         cursor-pointer text-1xl font-medium "
-        >
-          Sign Up
-        </button>
+          >
+            Sign Up
+          </button>
+        </div>
       </div>
     </div>
   );
